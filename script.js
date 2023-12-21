7a43d4453ee1918417dd89ac2027f6d46fad9031
Iris van Willigen <iris.van.willigen@gmail.com>
2023-12-21 13:39:59 +0100

    Add initial files. Add pseudo-code to js file.

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Define function getComputerChoice.
+Return Rock, Paper or Scissors.
+
+Ask player to select either rock, paper or scissors.
+Compare against computer choice. 
+Declare the winner.
+
+If tie, replay the round.
+
+Function that keeps score.
+Loop. End loop if either player or computer scores more than three.*/
+
+
+
